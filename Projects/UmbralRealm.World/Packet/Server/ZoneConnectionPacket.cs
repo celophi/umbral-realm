f4a6b55b703f82d4fefeee51ff84aafe0a0099cc
celophi <celophi@dakko.me>
2022-12-25 15:55:49 -0800

    Added packet model for zone connection information (#10)

---
+++ Projects/UmbralRealm.World/Packet/Server/ZoneConnectionPacket.cs
@@ -0,0 +1,32 @@
+﻿using BinarySerialization;
+using UmbralRealm.Core.Network.Packet;
+using UmbralRealm.Core.Network.Packet.Interfaces;
+
+namespace UmbralRealm.World.Packet.Server
+{
+    /// <summary>
+    /// World IP address and port information sent from the server to the client upon authenticating to the world server.
+    /// </summary>
+    [PacketOpcodeMapping((ushort)PacketOpcode.ZoneConnection)]
+    public class ZoneConnectionPacket : IPacket
+    {
+        /// <summary>
+        /// Unknown. Seems unused?
+        /// </summary>
+        [FieldOrder(0)]
+        public uint Unknown1 { get; set; }
+
+        [FieldOrder(1)]
+        public uint Unknown2 { get; set; }
+
+        [FieldOrder(2)]
+        public uint ZoneIPAddress { get; set; }
+
+        [FieldOrder(3)]
+        public ushort ZoneIPPort { get; set; }
+
+        [FieldOrder(4)]
+        [FieldLength(8)]
+        public byte[] Unknown3 { get; set; } = new byte[8];
+    }
+}
