f7c9f1c0db750480120c11415794f56877036c46
celophi <celophi@dakko.me>
2022-12-24 23:50:33 -0800

    Added packet model for world login acknowledgement

---
+++ Projects/UmbralRealm.World/Packet/Server/WorldLoginAcknowledgePacket.cs
@@ -0,0 +1,15 @@
+﻿using BinarySerialization;
+using UmbralRealm.Core.Network.Packet;
+
+namespace UmbralRealm.World.Packet.Server
+{
+    [PacketOpcodeMapping((ushort)PacketOpcode.WorldLoginAcknowledge)]
+    public class WorldLoginAcknowledgePacket
+    {
+        /// <summary>
+        /// This does not seem used.
+        /// </summary>
+        [FieldOrder(0)]
+        public uint Unused { get; set; }
+    }
+}
